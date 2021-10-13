7b7783df2dc3b6c93d99b4881bcfc6f8b5850502
Michał Sypniewski <mi.sypniewski@gmail.com>
2021-10-13 15:36:02 +0200

    feat: add all logic

---
+++ components/person.js
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react";
+import moment from "moment";
+
+const Person = ({ name }) => {
+  moment.locale("pl");
+  // const startTime = moment.now();
+  // const endTime = moment(startTime).add(8, "hours");
+
+  const [status, setStatus] = useState("PRZYJŚCIE");
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
+  const [time, setTime] = useState(" ");
+  const [intervalID, setIntervalID] = useState(null);
+
+  useEffect(() => {
+    if (moment(time).format("HH:mm:ss") === "00:00:00") {
+      // console.log(`OVERTIME!`);
+      clearInterval(intervalID);
+      setStatus("WYJŚCIE");
+      setTime((prevState) => {
+        const newState = moment(prevState).add(1, "seconds");
+        return newState;
+      });
+      UpTimer();
+    }
+  });
+
+  const changeStatus = (status) => {
+    if (status === "PRZYJŚCIE") {
+      const timeNow = moment.now();
+      // const timeEnd = moment(timeNow).add(10, "seconds");
+      const timeEnd = moment(timeNow).add(8, "hours");
+      const tmp = moment(timeEnd).subtract(timeNow);
+      const remanig = moment(tmp).subtract(1, "hour");
+      setStartTime(timeNow);
+      setEndTime(timeEnd);
+      setTime(remanig);
+      DownTimer();
+      setStatus("WCZEŚNIEJSZE WYJŚCIE");
+    }
+    if (status === "WCZEŚNIEJSZE WYJŚCIE") {
+      setEndTime(moment.now());
+      clearInterval(intervalID);
+      setStatus("WYJŚCIE");
+    }
+    if (status === "WYJŚCIE") {
+      setEndTime(moment.now());
+      clearInterval(intervalID);
+    }
+  };
+
+  const UpTimer = () => {
+    const intervalID = setInterval(() => {
+      setTime((prevState) => {
+        const newState = moment(prevState).add(1, "seconds");
+        return newState;
+      });
+    }, 1000);
+    setIntervalID(intervalID);
+  };
+
+  const DownTimer = () => {
+    const intervalID = setInterval(() => {
+      setTime((prevState) => {
+        const newState = moment(prevState).subtract(1, "seconds");
+        return newState;
+      });
+    }, 1000);
+    setIntervalID(intervalID);
+  };
+
+  return (
+    <div className="w-full h-48 rounded-lg bg-indigo-300 text-center p-2 shadow-xl">
+      <h2 className="mt-2 text-2xl font-bold">{name}</h2>
+      <p className="py-1 text-3xl mt-1">
+        {time !== " " ? moment(time).format("HH:mm:ss") : moment().format("DD-MM-YYYY")}
+      </p>
+      <div className="flex py-3 mt-1">
+        {status === "PRZYJŚCIE" ? (
+          <button
+            value={status}
+            onClick={(e) => changeStatus(e.target.value)}
+            className="block w-40 h-16 rounded-lg font-bold text-white shadow-lg mx-auto px-4 py-2 bg-green-600 hover:bg-green-700"
+          >
+            {status}
+          </button>
+        ) : null}
+        {status === "WCZEŚNIEJSZE WYJŚCIE" ? (
+          <button
+            value={status}
+            onClick={(e) => changeStatus(e.target.value)}
+            className="block w-40 h-16 rounded-lg font-bold text-white shadow-lg mx-auto px-4 py-2 bg-red-600 hover:bg-red-700"
+          >
+            {status}
+          </button>
+        ) : null}
+        {status === "WYJŚCIE" ? (
+          <button
+            value={status}
+            onClick={(e) => changeStatus(e.target.value)}
+            className="block w-40 h-16 rounded-lg font-bold text-white shadow-lg mx-auto px-4 py-2 bg-green-600 hover:bg-green-700"
+          >
+            {status}
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Person;
--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,33 @@
+import Person from "../components/person";
+import Clock from "../components/clock";
+
 export default function Home() {
+  const userList = [
+    "Grzegorz Sobstyl ",
+    "Kamil Milczarek",
+    "Radosław Przybyła",
+    "Michał Sypniewski",
+    "Łukasz Baranowski",
+    "Łukasz Grześkowiak",
+    "Rafał Kuźma",
+    "Piotr Szulkowski",
+    "Mateusz Mikołajczak",
+    "Szymon Ziemak",
+  ];
   return (
     <div className="lg:container mx-auto bg-white">
-      <h1 className="text-center font-bold text-3xl py-1 px-2">Punktualnik</h1>
-      <table className="table-fixed  text-center">
-        <thead>
-          <tr>
-            <th className="w-2/5 px-3">Pracownik</th>
-            <th className="w-2/5 px-3">Pozostały czas:</th>
-            <th className="w-1/5 px-3">Przyjście:</th>
-            <th className="w-1/5 px-3">Wyjście:</th>
-          </tr>
-        </thead>
-        <tbody className="font-mono">
-          <tr className="hover:bg-blue-300">
-            <td className="py-3 ">Michał Sypniewski</td>
-            <td>07:52:00</td>
-            <td>
-              <button className="block w-28 rounded-lg font-bold shadow-lg mx-auto px-4 py-2 bg-green-500 hover:bg-green-600">
-                START
-              </button>
-            </td>
-            <td>
-              <button className="block w-28 rounded-lg font-bold shadow-lg mx-auto px-4 py-2 bg-red-500 hover:bg-red-600">
-                STOP
-              </button>
-            </td>
-          </tr>
-          <tr className="hover:bg-blue-300">
-            <td className="py-3 ">Jacek Tomczak</td>
-            <td>07:52:00</td>
-            <td>
-              <button className="block w-20 rounded-lg font-bold shadow-lg mx-auto px-4 py-1 bg-green-500 hover:bg-green-600">
-                06:45
-              </button>
-            </td>
-            <td>
-              <button className="block w-20 rounded-lg font-bold shadow-lg mx-auto px-4 py-1 bg-red-500 hover:bg-red-600">
-                STOP
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <div className="my-56 grid grid-cols-3 gap-2">
-        <div className="w-full h-40 rounded-lg bg-blue-300 text-center p-2">
-          <h2 className="py-1 text-2xl">Jacek Tomczak</h2>
-          <p className="py-1 text-4xl">07:35:00</p>
-          <div className="flex py-3">
-            <button className="block w-20 rounded-lg font-bold shadow-lg mx-auto px-4 py-1 bg-green-500 hover:bg-green-600">
-              START
-            </button>
-            <button className="block w-20 rounded-lg font-bold shadow-lg mx-auto px-4 py-1 bg-red-500 hover:bg-red-600">
-              STOP
-            </button>
-          </div>
-        </div>
-        <div className="w-full h-40 rounded-lg bg-blue-300 text-center p-2">
-          <h2 className="py-1 text-2xl">Jacek Tomczak</h2>
-          <p className="py-1 text-4xl">07:35:00</p>
-          <div className="flex py-3">
-            <button className="block w-20 rounded-lg font-bold shadow-lg mx-auto px-4 py-1 bg-green-500 hover:bg-green-600">
-              START
-            </button>
-            <button className="block w-20 rounded-lg font-bold shadow-lg mx-auto px-4 py-1 bg-red-500 hover:bg-red-600">
-              STOP
-            </button>
-          </div>
-        </div>
-        <div className="w-full h-40 rounded-lg bg-blue-300 text-center p-2">
-          <h2 className="py-1 text-2xl">Jacek Tomczak</h2>
-          <p className="py-1 text-4xl">07:35:00</p>
-          <div className="flex py-3">
-            <button className="block w-20 rounded-lg font-bold shadow-lg mx-auto px-4 py-1 bg-green-500 hover:bg-green-600">
-              START
-            </button>
-            <button className="block w-20 rounded-lg font-bold shadow-lg mx-auto px-4 py-1 bg-red-500 hover:bg-red-600">
-              STOP
-            </button>
-          </div>
-        </div>
-        <div className="w-full h-40 rounded-lg bg-blue-300 text-center p-2">
-          <h2 className="py-1 text-2xl">Jacek Tomczak</h2>
-          <p className="py-1 text-4xl">07:35:00</p>
-          <div className="flex py-3">
-            <button className="block w-20 rounded-lg font-bold shadow-lg mx-auto px-4 py-1 bg-green-500 hover:bg-green-600">
-              START
-            </button>
-            <button className="block w-20 rounded-lg font-bold shadow-lg mx-auto px-4 py-1 bg-red-500 hover:bg-red-600">
-              STOP
-            </button>
-          </div>
-        </div>
-        <div className="w-full h-40 rounded-lg bg-blue-300 text-center p-2">
-          <h2 className="py-1 text-2xl">Jacek Tomczak</h2>
-          <p className="py-1 text-4xl">07:35:00</p>
-          <div className="flex py-3">
-            <button className="block w-28 rounded-lg font-bold shadow-lg mx-auto px-4 py-2 bg-green-500 hover:bg-green-600">
-              START
-            </button>
-          </div>
-        </div>
-        <div className="w-full h-40 rounded-lg bg-blue-300 text-center p-2">
-          <h2 className="py-1 text-2xl">Jacek Tomczak</h2>
-          <p className="py-1 text-4xl">07:35:00</p>
-          <div className="flex py-3">
-            <button className="block w-28 rounded-lg font-bold shadow-lg mx-auto px-4 py-2 bg-red-500 hover:bg-red-600">
-              STOP
-            </button>
-          </div>
-        </div>
+      {/* <h1 className="text-center font-bold text-3xl py-1 px-2">Punktualnik</h1> */}
+      {/* <p className="text-center">13-10-2021 09:11</p> */}
+      <div className="grid grid-cols-3 gap-4 mt-4">
+        {userList.map((user) => (
+          <Person name={user} key={user} />
+        ))}
       </div>
+      {/* <div className="grid grid-cols-3 gap-4 justify-items-center mt-4">
+        {userList.map((user) => (
+          <Clock name={user} key={user} />
+        ))}
+      </div> */}
     </div>
   );
 }
