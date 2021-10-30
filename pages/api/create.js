de865d3ab012d311d287c6e9ad57028b4b29b852
Michal Sypniewski <mi.sypniewski@gmail.com>
2021-10-30 20:24:46 +0200

    feat: add new api point

---
+++ pages/api/create.js
@@ -0,0 +1,22 @@
+import newDay from "../../services/newDay";
+
+export default async (req, res) => {
+  switch (req.method) {
+    case "GET": {
+      const newDays = await newDay();
+      res.status(200).json(newDays);
+
+      break;
+    }
+    // case "POST": {
+    //   const payload = req.body;
+    //   console.log("payload on backend", payload);
+    //   const offer = await saveTimes(payload);
+    //   res.status(200).json({ status: "created", offer });
+
+    //   break;
+    // }
+    default:
+      res.status(400);
+  }
+};
