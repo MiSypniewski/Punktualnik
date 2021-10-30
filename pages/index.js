55cc6584bfc88a03b3d70bd4d1ccbccd00bdb2ac
Michal Sypniewski <mi.sypniewski@gmail.com>
2021-10-30 16:39:00 +0200

    feat: change main page

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,28 @@ import useSWR from "swr";
 import { jsonFetcher } from "../utils";
 import moment from "moment";
 
-// export const getStaticProps = async () => {
+export const getStaticProps = async () => {
+  const times = await getRecentTimes(moment().format("DD-MM-YYYY"));
+  const users = await getUsers("Biedronka");
+
+  //zmemić na pobieranie lokalizacji , działów lub możliwość wszystkich!!!
+
+  users.forEach((user) => {
+    user.Password = ":)";
+    user.SaltPassword = ":)";
+    return user;
+  });
+
+  console.log(users);
+  return {
+    props: {
+      times,
+      users,
+    },
+  };
+};
+
+// export const getServerSideProps = async () => {
 //   const times = await getRecentTimes(moment().format("DD-MM-YYYY"));
 //   const users = await getUsers("Biedronka");
 
@@ -25,35 +46,22 @@ import moment from "moment";
 //   };
 // };
 
-export const getServerSideProps = async () => {
-  const times = await getRecentTimes(moment().format("DD-MM-YYYY"));
-  const users = await getUsers("Biedronka");
-
-  users.forEach((user) => {
-    user.Password = ":)";
-    user.SaltPassword = ":)";
-    return user;
-  });
-
-  console.log(users);
-  return {
-    props: {
-      times,
-      users,
-    },
-  };
-};
-
 export default function Home({ times, users }) {
   const { data } = useSWR("/api/times", jsonFetcher, { initialData: times });
   // console.log(data);
   console.log(users);
   return (
     <div className="lg:container mx-auto bg-white">
-      {/* <h1 className="text-center font-bold text-3xl py-1 px-2">Punktualnik</h1> */}
+      <h1 className="text-center font-bold text-3xl py-1 px-2">Punktualnik</h1>
       {/* <p className="text-center">13-10-2021 09:11</p> */}
       <div className="grid grid-cols-3 gap-4 mt-4">
-        {users.map((user) => (
+        <button
+          onClick={() => console.log(`przeniesie do sekcji!`)}
+          className="block w-40 h-16 rounded-lg font-bold text-white text-lg shadow-lg mx-auto px-4 py-2 bg-red-600 hover:bg-red-700"
+        >
+          Biedronka
+        </button>
+        {/* {users.map((user) => (
           <Person
             name={user.Name}
             surname={user.Surname}
@@ -61,7 +69,7 @@ export default function Home({ times, users }) {
             section={user.Section}
             key={user.ID}
           />
-        ))}
+        ))} */}
       </div>
       {/* <div className="grid grid-cols-3 gap-4 justify-items-center mt-4">
         {userList.map((user) => (
