d7f66cbfbfe72be2e9813c11f1e558d29b87f049
Michał Sypniewski <mi.sypniewski@gmail.com>
2021-11-27 18:28:55 +0100

    feat: disable create new day from api

--- pages/api/create.js
+++ pages/api/create.js
@@ -8,17 +8,17 @@ export default async (req, res) => {
 
     //   break;
     // }
-    case "POST": {
-      const payload = req.body;
-      if (payload.text === "punktuanalnik") {
-        console.log("payload on backend", payload);
-        const offer = await newDay();
-        res.status(200).json({ status: "created", offer });
-      } else {
-        res.status(200).json({ status: "error" });
-      }
-      break;
-    }
+    // case "POST": {
+    //   const payload = req.body;
+    //   if (payload.text === "punktuanalnik") {
+    //     console.log("payload on backend", payload);
+    //     const offer = await newDay();
+    //     res.status(200).json({ status: "created", offer });
+    //   } else {
+    //     res.status(200).json({ status: "error" });
+    //   }
+    //   break;
+    // }
     default:
       res.status(400);
   }
