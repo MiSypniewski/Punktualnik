ab5cf5f2f1b1fef9108a392c9a70e88f8137ed26
Michał Sypniewski <mi.sypniewski@gmail.com>
2021-11-27 18:28:22 +0100

    feat: add auto refresh page

--- pages/time/[id].js
+++ pages/time/[id].js
@@ -1,4 +1,5 @@
-import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import router, { useRouter } from "next/router";
 import Person from "../../components/person";
 import getSectionTime from "../../services/getSectionTime";
 import useSWR from "swr";
@@ -26,17 +27,38 @@ export default function Home({ times, id }) {
   // router.push(`/`);
   // }
 
-  // const { data } = useSWR(`/api/section/${id}`, jsonFetcher, { initialData: times });
+  const { data } = useSWR(`/api/section/${id}`, jsonFetcher, { initialData: times });
   // console.log(`data:`);
   // console.log(data);
   // console.log(times);
+  const [intervalID, setIntervalID] = useState(null);
+  const [firtstRun, setFirstRun] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    if (!firtstRun) {
+      setInterval(() => {
+        if (moment().format("HH:mm:ss") === "03:15:00") router.reload();
+        setCounter((prevState) => {
+          const newState = (prevState += 1);
+          return newState;
+        });
+      }, 1000);
+      setIntervalID(intervalID);
+      setFirstRun(true);
+    }
+  }, [firtstRun]);
+
   return (
     <div className="lg:container mx-auto bg-white">
-      {/* <h1 className="text-center font-bold text-3xl py-1 px-2">Punktualnik</h1> */}
+      {/* <h1 className="text-center font-bold text-3xl py-1 px-2">{firtstRun ? "Prawda" : "Fałsz"}</h1> */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-1 p-2">
-        {times.map((time) => (
+        {data
+          ? data.map((time) => <Person time={time} key={time.ID} tmp="dataMap" />)
+          : times.map((time) => <Person time={time} key={time.ID} tmp="timesMap" />)}
+        {/* {times.map((time) => (
           <Person time={time} key={time.ID} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
