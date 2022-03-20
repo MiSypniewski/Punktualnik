a71a44b4bcb1c5e50291b9c2684b4a21eac216a4
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-03-20 10:05:55 +0100

    feat: add links to project "tablet"

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Person from "../components/person";
 import Clock from "../components/clock";
+import Link from "next/link";
 // import getRecentTimes from "../services/getTime";
 // import getUsers from "../services/getUsers";
 import useSWR from "swr";
@@ -43,15 +44,42 @@ export default function Home({}) {
   // console.log(users);
   return (
     <div className="lg:container mx-auto bg-white">
-      <h1 className="text-center font-bold text-3xl py-1 px-2">Punktualnik</h1>
+      <h1 className="text-center font-bold text-3xl py-1 px-2">opss.pl</h1>
       {/* <p className="text-center">13-10-2021 09:11</p> */}
-      <div className="grid grid-cols-3 gap-4 mt-4">
-        <button
+      <div className="lg:container mx-auto grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4 mt-1 p-2">
+        <Link href="https://drive.google.com/file/d/1P4WBxbB-dxMPKv8f26-_WFZ2hv0u03c9/view?usp=sharing">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="block w-40 h-16 rounded-lg font-bold text-white text-center leading-10 text-lg shadow-lg mx-auto px-4 py-2 bg-green-600 hover:bg-green-700"
+          >
+            Tapeta
+          </a>
+        </Link>
+        <Link href="https://drive.google.com/file/d/1BM7vNgM6djdokPgp3aaF9aUzDPF9Ce5V/view?usp=sharing">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="block w-40 h-16 rounded-lg font-bold text-white text-center leading-10 text-lg shadow-lg mx-auto px-4 py-2 bg-blue-600 hover:bg-blue-700"
+          >
+            Wygaszacz
+          </a>
+        </Link>
+        <Link href="https://drive.google.com/file/d/1Egsu6Ox7mxuDCG4R9WWalDrC0ftAmW9E/view?usp=sharing">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="block w-40 h-16 rounded-lg font-bold text-white text-center leading-10 text-lg shadow-lg mx-auto px-4 py-2 bg-red-600 hover:bg-red-700"
+          >
+            Aplikacja
+          </a>
+        </Link>
+        {/* <button
           onClick={() => console.log(`przeniesie do sekcji!`)}
           className="block w-40 h-16 rounded-lg font-bold text-white text-lg shadow-lg mx-auto px-4 py-2 bg-red-600 hover:bg-red-700"
         >
           Biedronka
-        </button>
+        </button> */}
         {/* {users.map((user) => (
           <Person
             name={user.Name}
