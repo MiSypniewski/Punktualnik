365c8dd6f2d91a64c36093299d17f39fa1be36f7
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-05-19 19:09:34 +0200

    fix: check existing user

--- services/createUser.js
+++ services/createUser.js
@@ -8,30 +8,42 @@ const schema = Joi.object({
   surname: Joi.string().required(),
   section: Joi.string().required(),
   location: Joi.string().required(),
-  login: Joi.string().required(),
   password: Joi.string().required(),
 });
 
-const create = async (payload) => {
-  const { email, name, surname, section, location, login, password } = await schema.validateAsync(payload);
+const checkEmail = async (email) => {
+  const existingUser = await airDB("Users")
+    .select({ filterByFormula: `email="${email}"` })
+    .firstPage();
+  if (existingUser && existingUser[0]) {
+    throw new Error("email_taken");
+  }
+};
+
+const createUser = async (payload) => {
+  const { email, name, surname, section, location, password } = await schema.validateAsync(payload);
+  await checkEmail(email);
+
   const passwordSalt = crypto.randomBytes(256).toString("hex");
   const passwordHash = crypto.pbkdf2Sync(password, passwordSalt, 2137, 256, "sha512").toString("hex");
 
-  const user = await airDB("users").create([
+  const user = await airDB("Users").create([
     {
       fields: {
-        email,
         name,
         surname,
         section,
         location,
-        login,
-        passwordSalt,
+        email,
         passwordHash,
+        passwordSalt,
         role: "user",
+        isActive: "Inactive",
       },
     },
   ]);
 
   return user;
 };
+
+export default createUser;
