78d0649e778f20baf08333f2f60e3eee691486f4
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-05-22 20:17:44 +0200

    feat: add BaseLayout component

---
+++ components/baseLayout.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
+import moment from "moment";
+import { getCurrentTime } from "../utils";
+
+const TopNavigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
+  const [intervalID, setIntervalID] = useState(null);
+  const [firtstRun, setFirstRun] = useState(false);
+  const [counter, setCounter] = useState(0);
+
+  console.log(`session:`);
+  console.log(session);
+  moment.locale("pl");
+  const date = moment().locale("pl").format("dddd, D MMMM YYYY, ");
+  const time = getCurrentTime();
+
+  useEffect(() => {
+    if (!firtstRun) {
+      setInterval(() => {
+        if (moment().format("HH:mm:ss") === "03:15:00") router.reload();
+        setCounter((prevState) => {
+          const newState = (prevState += 1);
+          return newState;
+        });
+      }, 1000);
+      setIntervalID(intervalID);
+      setFirstRun(true);
+    }
+  }, [firtstRun]);
+
+  return (
+    <div className="flex gap-6 w-full px-4 py-1">
+      <p className="capitalize flex-grow font-bold">{`${date} ${time}`}</p>
+      {/* <p className="text-center flex-grow">Pysznej kawusi...</p> */}
+      <p className="capitalize font-bold">{session ? session.section : "Zaloguj się"}</p>
+    </div>
+  );
+};
+
+export default function BaseLayout({ children }) {
+  return (
+    <>
+      <TopNavigation />
+      {children}
+      {/* <Footer /> */}
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Link from "next/link";
 import useSWR from "swr";
 import { jsonFetcher } from "../utils";
 import moment from "moment";
+import BaseLayout from "../components/baseLayout";
 
 // export const getStaticProps = async () => {
 // const times = await getRecentTimes(moment().format("DD-MM-YYYY"));
@@ -43,44 +44,45 @@ export default function Home({}) {
   // console.log(data);
   // console.log(users);
   return (
-    <div className="lg:container mx-auto bg-white">
-      <h1 className="text-center font-bold text-3xl py-1 px-2">opss.pl</h1>
-      {/* <p className="text-center">13-10-2021 09:11</p> */}
-      <div className="lg:container mx-auto grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4 mt-1 p-2">
-        <Link href="https://drive.google.com/file/d/1P4WBxbB-dxMPKv8f26-_WFZ2hv0u03c9/view?usp=sharing">
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="block w-40 h-16 rounded-lg font-bold text-white text-center leading-10 text-lg shadow-lg mx-auto px-4 py-2 bg-green-600 hover:bg-green-700"
-          >
-            Tapeta
-          </a>
-        </Link>
-        <Link href="https://drive.google.com/file/d/1BM7vNgM6djdokPgp3aaF9aUzDPF9Ce5V/view?usp=sharing">
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="block w-40 h-16 rounded-lg font-bold text-white text-center leading-10 text-lg shadow-lg mx-auto px-4 py-2 bg-blue-600 hover:bg-blue-700"
-          >
-            Wygaszacz
-          </a>
-        </Link>
-        <Link href="https://drive.google.com/file/d/1Egsu6Ox7mxuDCG4R9WWalDrC0ftAmW9E/view?usp=sharing">
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="block w-40 h-16 rounded-lg font-bold text-white text-center leading-10 text-lg shadow-lg mx-auto px-4 py-2 bg-red-600 hover:bg-red-700"
-          >
-            Aplikacja
-          </a>
-        </Link>
-        {/* <button
+    <BaseLayout>
+      <div className="lg:container mx-auto bg-white">
+        <h1 className="text-center font-bold text-3xl py-1 px-2">opss.pl</h1>
+        {/* <p className="text-center">13-10-2021 09:11</p> */}
+        <div className="lg:container mx-auto grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4 mt-1 p-2">
+          <Link href="https://drive.google.com/file/d/1P4WBxbB-dxMPKv8f26-_WFZ2hv0u03c9/view?usp=sharing">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="block w-40 h-16 rounded-lg font-bold text-white text-center leading-10 text-lg shadow-lg mx-auto px-4 py-2 bg-green-600 hover:bg-green-700"
+            >
+              Tapeta
+            </a>
+          </Link>
+          <Link href="https://drive.google.com/file/d/1BM7vNgM6djdokPgp3aaF9aUzDPF9Ce5V/view?usp=sharing">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="block w-40 h-16 rounded-lg font-bold text-white text-center leading-10 text-lg shadow-lg mx-auto px-4 py-2 bg-blue-600 hover:bg-blue-700"
+            >
+              Wygaszacz
+            </a>
+          </Link>
+          <Link href="https://drive.google.com/file/d/1Egsu6Ox7mxuDCG4R9WWalDrC0ftAmW9E/view?usp=sharing">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="block w-40 h-16 rounded-lg font-bold text-white text-center leading-10 text-lg shadow-lg mx-auto px-4 py-2 bg-red-600 hover:bg-red-700"
+            >
+              Aplikacja
+            </a>
+          </Link>
+          {/* <button
           onClick={() => console.log(`przeniesie do sekcji!`)}
           className="block w-40 h-16 rounded-lg font-bold text-white text-lg shadow-lg mx-auto px-4 py-2 bg-red-600 hover:bg-red-700"
         >
           Biedronka
         </button> */}
-        {/* {users.map((user) => (
+          {/* {users.map((user) => (
           <Person
             name={user.Name}
             surname={user.Surname}
@@ -89,12 +91,13 @@ export default function Home({}) {
             key={user.ID}
           />
         ))} */}
-      </div>
-      {/* <div className="grid grid-cols-3 gap-4 justify-items-center mt-4">
+        </div>
+        {/* <div className="grid grid-cols-3 gap-4 justify-items-center mt-4">
         {userList.map((user) => (
           <Clock name={user} key={user} />
         ))}
       </div> */}
-    </div>
+      </div>
+    </BaseLayout>
   );
 }
