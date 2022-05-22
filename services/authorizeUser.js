0e1bba95be1c1d79b4f1eaf034aeb05f93284ab5
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-05-22 18:14:16 +0200

    feat: add authorization user

---
+++ services/authorizeUser.js
@@ -0,0 +1,42 @@
+// import airDB from "services/airtableClient";
+import airDB from "./airtableClient";
+import Joi from "joi";
+import crypto from "crypto";
+
+const schema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+const authorizeUser = async (payload) => {
+  const { email, password } = await schema.validateAsync(payload);
+
+  const [user] = await airDB("Users")
+    .select({ filterByFormula: `email="${email}"` })
+    .firstPage();
+
+  if (!user) {
+    const passwordSalt = crypto.randomBytes(256).toString("hex");
+    const passwordHash = crypto.pbkdf2Sync(password, passwordSalt, 2137, 256, `sha512`).toString(`hex`);
+    return null;
+  }
+
+  const passwordHash = crypto.pbkdf2Sync(password, user.fields.passwordSalt, 2137, 256, `sha512`).toString(`hex`);
+
+  if (passwordHash !== user.fields.passwordHash) {
+    return null;
+  }
+
+  if (user.fields.isActive === "Inactive") {
+    return null;
+  }
+
+  return {
+    id: user.id,
+    email: user.fields.email,
+    name: user.fields.name,
+    role: user.fields.role,
+  };
+};
+
+export default authorizeUser;
