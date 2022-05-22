b7150b37e89ff96362b1274ea7723119af76a83b
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-05-22 20:17:04 +0200

    feat: add getCurrentTime function

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,8 @@
+import moment from "moment";
+
 export const jsonFetcher = (url) => fetch(url).then((res) => res.json());
+
+export const getCurrentTime = () => {
+  moment.locale("pl");
+  return moment().format("HH:mm:ss");
+};
