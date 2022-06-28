370ac2f094d4bc652352dead3e629b54765daa03
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-06-28 19:19:51 +0200

    feat: add session check in create users

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,10 +1,17 @@
 import createUser from "../../../services/createUser";
+import { getSession } from "next-auth/react";
 // import authorizeUser from "../../../services/authorize";
 
 export default async (req, res) => {
   switch (req.method) {
     case "POST": {
       try {
+        // console.log(req);
+        const session = await getSession({ req });
+        if (!session) {
+          return res.status(401).json({ error: "not_authotized" });
+        }
+
         const payload = req.body;
         const user = await createUser(payload);
         res.status(200).json({ status: "created", user });
