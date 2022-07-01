a9cb9d800637720765c4a60f9233b26e89494405
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-07-01 08:19:58 +0200

    feat: install dayjs - replace momentjs

--- components/baseLayout.js
+++ components/baseLayout.js
@@ -4,6 +4,9 @@ import { signOut, useSession } from "next-auth/react";
 import moment from "moment";
 import { getCurrentTime } from "../utils";
 import { useRouter } from "next/router";
+import dayjs from "dayjs";
+import "dayjs/locale/pl";
+dayjs.locale("pl");
 
 const TopNavigation = ({ section }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,6 +15,7 @@ const TopNavigation = ({ section }) => {
   const [intervalID, setIntervalID] = useState(null);
   const [firtstRun, setFirstRun] = useState(false);
   const [counter, setCounter] = useState(0);
+  // dayjs.locale("pl");
 
   // console.log(loading);
 
@@ -19,24 +23,24 @@ const TopNavigation = ({ section }) => {
   // const date = moment().locale("pl").format("dddd, D MMMM YYYY, ");
   // const time = getCurrentTime();
 
-  // useEffect(() => {
-  //   if (!firtstRun) {
-  //     setInterval(() => {
-  //       if (moment().format("HH:mm:ss") === "03:15:00") router.reload();
-  //       setCounter((prevState) => {
-  //         const newState = (prevState += 1);
-  //         return newState;
-  //       });
-  //     }, 1000);
-  //     setIntervalID(intervalID);
-  //     setFirstRun(true);
-  //   }
-  // }, [firtstRun]);
+  useEffect(() => {
+    if (!firtstRun) {
+      setInterval(() => {
+        if (dayjs().format("HH:mm:ss") === "03:15:00") router.reload();
+        setCounter((prevState) => {
+          const newState = (prevState += 1);
+          return newState;
+        });
+      }, 1000);
+      setIntervalID(intervalID);
+      setFirstRun(true);
+    }
+  }, [firtstRun]);
 
   return (
     <div className="flex gap-6 w-full px-4 py-1">
       {/* <p className="capitalize flex-grow font-bold">{`${moment().format("dddd, D MMMM YYYY, HH:mm:ss")}`}</p> */}
-      <p className="capitalize flex-grow font-bold">{`Tutaj będzie data i godzina`}</p>
+      <p className="capitalize flex-grow font-bold">{dayjs().format(`dddd, DD MMMM YYYY, HH:mm:ss `)}</p>
       {/* <p className="text-center flex-grow">Pysznej kawusi...</p> */}
       <p className="capitalize font-bold">{section}</p>
     </div>
