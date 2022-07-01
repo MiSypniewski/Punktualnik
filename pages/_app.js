70945b731d1529adbbccd44ab5b8319ae00ae56d
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-07-01 08:20:18 +0200

    fix: session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
-
-function MyApp({ Component, pageProps }) {
+//, pageProps: { session, ...pageProps }
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={pageProps.session}>
       <Head>
