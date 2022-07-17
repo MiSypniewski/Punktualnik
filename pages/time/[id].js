1230317b592319e5ca465fba212b6cf82d3ecb23
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-07-17 22:36:23 +0200

    fix: clear code

--- pages/time/[id].js
+++ pages/time/[id].js
@@ -1,69 +1,33 @@
-import { useState, useEffect } from "react";
-import router, { useRouter } from "next/router";
-import Person from "../../components/person";
-import NewPerson from "../../components/newPerson";
 import getSectionTime from "../../services/getSectionTime";
 import useSWR from "swr";
-import { jsonFetcher } from "../../utils";
-import moment from "moment";
 import BaseLayout from "../../components/baseLayout";
-
-moment.locale("pl");
+import Card from "../../components/card";
+import dayjs from "dayjs";
+import "dayjs/locale/pl";
+dayjs.locale("pl");
 
 export const getServerSideProps = async (context) => {
-  moment.locale("pl");
-  const now = moment().hours(0).minutes(0).seconds(0).milliseconds(0).format();
-  const data = moment(now).hours(3).format();
-  const times = await getSectionTime(context.params.id, data);
-  const date = moment().format("dddd, D MMMM YYYY, ");
-  // console.log(times);
+  const toDay = dayjs().hour(3).minute(0).second(0).millisecond(0).format();
+  const cardData = await getSectionTime(context.params.id, toDay);
 
   return {
     props: {
-      date,
-      times,
+      cardData,
       id: context.params.id,
     },
   };
 };
 
-export default function Home({ times, id, date }) {
-  moment.locale("pl");
-  const time = moment().format("HH:mm:ss");
-
-  // const router = useRouter();
-  // if (!users.length) {
-  // router.push(`/`);
-  // }
-  const { data } = useSWR(`/api/section/${id}`, jsonFetcher, { initialData: times });
-  // const { data } = useSWR(`/api/section/${id}`, jsonFetcher);
-  const [intervalID, setIntervalID] = useState(null);
-  const [firtstRun, setFirstRun] = useState(false);
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    if (!firtstRun) {
-      setInterval(() => {
-        if (moment().format("HH:mm:ss") === "03:15:00") router.reload();
-        setCounter((prevState) => {
-          const newState = (prevState += 1);
-          return newState;
-        });
-      }, 1000);
-      setIntervalID(intervalID);
-      setFirstRun(true);
-    }
-  }, [firtstRun]);
+export default function Home({ cardData, id }) {
+  // const { data } = useSWR(`/api/section/${id}`, jsonFetcher, { initialData: cardData });
+  const data = undefined;
 
   return (
     <BaseLayout>
       <div className="lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-1 p-2">
         {data != undefined
-          ? data.map((user) => <NewPerson time={user} key={time.ID} />)
-          : times.map((user) => <NewPerson time={user} key={time.ID} />)}
-        {/* {times.map((time) => (
-          <NewPerson time={time} key={time.ID} />
-        ))} */}
+          ? data.map((data) => <Card data={data} key={data.ID} />)
+          : cardData.map((data) => <Card data={data} key={data.ID} />)}
       </div>
     </BaseLayout>
   );
