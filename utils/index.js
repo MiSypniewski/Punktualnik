bea28eb602c5e5794bfa806e1d8bb5e3c41b680b
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-07-17 22:36:51 +0200

    feat: add new functions

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,43 @@
-import moment from "moment";
+import dayjs from "dayjs";
+import "dayjs/locale/pl";
+const AdvancedFormat = require("dayjs/plugin/advancedFormat");
+const isSameOrBefore = require("dayjs/plugin/isSameOrBefore");
+const duration = require("dayjs/plugin/duration");
+dayjs.extend(AdvancedFormat);
+dayjs.extend(isSameOrBefore);
+dayjs.extend(duration);
+dayjs.locale("pl");
+
+export const Timer = (stop) => {
+  if (dayjs().isSameOrBefore(stop, "seconds")) {
+    const tmp = dayjs.duration(dayjs(stop).diff(dayjs()));
+    return {
+      overtime: false,
+      time: tmp.format(`HH:mm:ss`),
+    };
+  } else {
+    const tmp = dayjs.duration(dayjs(dayjs()).diff(stop));
+    return {
+      overtime: true,
+      time: tmp.format(`HH:mm:ss`),
+    };
+  }
+};
+
+export const DifferenceTime = (start, stop) => {
+  const workTime = dayjs(stop).diff(dayjs(start), "hours");
+  const tmp = dayjs.duration(dayjs(stop).diff(dayjs(start)));
+  if (workTime < 8) {
+    return {
+      overtime: false,
+      time: tmp.format(`HH:mm:ss`),
+    };
+  } else {
+    return {
+      overtime: true,
+      time: tmp.format(`HH:mm:ss`),
+    };
+  }
+};
 
 export const jsonFetcher = (url) => fetch(url).then((res) => res.json());
-
-export const getCurrentTime = () => {
-  moment.locale("pl");
-  return moment().format("HH:mm:ss");
-};
