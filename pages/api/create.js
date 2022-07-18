c8ddaac47ea79fbe655b27232c78d6971a878729
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-07-18 20:32:55 +0200

    feat: add create time api

--- pages/api/create.js
+++ pages/api/create.js
@@ -1,25 +1,25 @@
 import newDay from "../../services/newDay";
 
 export default async (req, res) => {
-  // switch (req.method) {
-  //   case "GET": {
-  //     console.log(`metoda GET`);
-  //     const newDays = await newDay();
-  //     res.status(200).json(newDays);
-  //     break;
-  //   }
-  //   case "POST": {
-  //     const payload = req.body;
-  //     if (payload.text === "punktuanalnik") {
-  //       console.log("payload on backend", payload);
-  //       const offer = await newDay();
-  //       res.status(200).json({ status: "created", offer });
-  //     } else {
-  //       res.status(200).json({ status: "error" });
-  //     }
-  //     break;
-  //   }
-  //   default:
-  //     res.status(400);
-  // }
+  switch (req.method) {
+    case "GET": {
+      console.log(`metoda GET`);
+      const newDays = await newDay();
+      res.status(200).json(newDays);
+      break;
+    }
+    case "POST": {
+      const payload = req.body;
+      if (payload.text === "punktuanalnik") {
+        console.log("payload on backend", payload);
+        const offer = await newDay();
+        res.status(200).json({ status: "created", offer });
+      } else {
+        res.status(200).json({ status: "error" });
+      }
+      break;
+    }
+    default:
+      res.status(400);
+  }
 };
