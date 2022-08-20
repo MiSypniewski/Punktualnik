b6894878a97b84c66d39fb06068c026ad90552a5
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-08-20 12:35:19 +0200

    feat: add new logic to display cardData

--- pages/time/[id].js
+++ pages/time/[id].js
@@ -8,10 +8,11 @@ import "dayjs/locale/pl";
 dayjs.locale("pl");
 
 export const getServerSideProps = async (context) => {
+  //pobieranie dzisiejszej daty i ustawnianie godziny na 3 w nocy aby uniknąć problemów ze zmianą czasu na letni i zimowy
   const toDay = dayjs().hour(3).minute(0).second(0).millisecond(0).format();
-  //pobieranie użytkowników z danej sekcji (contex.params.id)
+  //pobieranie aktywnych użytkowników z danej sekcji (contex.params.id) -- tylko "user", żaden edytor
   const users = await getUsers(context.params.id);
-  //pobieranie z bazy dzisiejszych rekordów na podstawie użytkowników
+  //pobieranie z bazy dzisiejszych rekordów z danej sekcji, Jeżeli ktoś już zaznaczył swoją obecność będzie w tej bazie.
   const cardData = await getSectionTime(context.params.id, toDay);
   // pusta tablica na dane użytkowników
   const newCardData = [];
@@ -33,31 +34,46 @@ export const getServerSideProps = async (context) => {
       status: "wait",
       overTime: false,
     };
+    //dodawanie nowego pustego użytkownika.
     newCardData.push(emptyUser);
   };
 
-  // porównanie czy każdy użytkownik jest zapisany w dzisiejszej bazie.
-  if (Array.isArray(cardData) && !cardData.length) {
-    // brak zapisanego czasu ŻADNEGO użytkownika w danym dniu
+  // porównanie czy każdy aktywny użytkownik jest zapisany w bazie.
+
+  // brak zapisanego czasu ŻADNEGO użytkownika w danym dniu
+  if (Array.isArray(cardData) && cardData.length === 0) {
+    //dodawanie każdego aktywnego użytkownika
     users.forEach((user) => {
       addEmptyUser(user);
     });
   }
 
-  if (Array.isArray(cardData) && cardData.length) {
+  // znalezione czasy użytkowników w bazie
+  if (Array.isArray(cardData) && cardData.length > 0) {
+    console.log(`cardData pełne`);
+    // sprawdza który użytkownik z danej sekcji zapisaj już swój czas wejścia w bazie
     users.forEach((user) => {
-      // znalezione czasy użytkowników w bazie
-      // console.log(`sprawdzam użytkownika: `, user);
-      // sprawdza czy każdy użytkownik z danej sekcji zapisaj już swój czas wejścia
       cardData.forEach((card) => {
         if (user.ID === card.userID) {
           // znaleziony użytkownik
           newCardData.push(card);
-        } else {
-          // tworzenie pustego użytkownika
-          addEmptyUser(user);
         }
       });
+    });
+
+    // sprawdza który użytkownik jeszcze nie zapisał swojego czasu przyjścia w bazie
+    users.forEach((user) => {
+      let flag = true;
+      newCardData.forEach((newCard) => {
+        if (newCard.userID === user.ID) {
+          flag = false;
+        }
+      });
+
+      if (flag) {
+        addEmptyUser(user);
+        flag = true;
+      }
     });
   }
 
@@ -76,7 +92,6 @@ export default function Home({ newCardData, id }) {
   return (
     <BaseLayout>
       <div className="lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-1 p-2">
-        {/* problem z użytkownikami pustymi, brak data.ID jako unikatowy KEY */}
         {data != undefined
           ? data.map((data) => <Card data={data} key={data.ID} />)
           : newCardData.map((data) => <Card data={data} key={data.ID} />)}
