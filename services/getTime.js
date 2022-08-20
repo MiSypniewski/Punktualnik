ebcb588bc27cc943f1459df047b828f5a33855b8
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-08-20 12:38:10 +0200

    feat: add new logic to get time for simple user

--- services/getTime.js
+++ services/getTime.js
@@ -1,12 +1,21 @@
 import airDB from "./airtableClient";
+import dayjs from "dayjs";
+import "dayjs/locale/pl";
+dayjs.locale("pl");
 
-const getTime = async (userID, data) => {
+const getTime = async (userID) => {
+  const data = dayjs().hour(3).minute(0).second(0).millisecond(0).format();
   const times = await airDB("Times")
     .select({ filterByFormula: `AND(userID="${userID}", data="${data}")` })
     // .select({ filterByFormula: `Data="${data}"` })
     .firstPage();
 
-  return times.map((time) => time.fields);
+  return times.map((time) => {
+    return {
+      airtableID: time.id,
+      ...time.fields,
+    };
+  });
 };
 
 export default getTime;
