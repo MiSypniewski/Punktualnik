c69d4c18899a4ba1346becc932919cbd3d234202
Michał Sypniewski <mi.sypniewski@gmail.com>
2022-09-14 20:57:10 +0200

    feat: check user permision

--- pages/api/time/[id].js
+++ pages/api/time/[id].js
@@ -1,6 +1,7 @@
 import getTime from "../../../services/getTime";
 import saveTimes from "../../../services/saveTime";
 import updateTime from "../../../services/updateTime";
+import { getSession } from "next-auth/react";
 import dayjs from "dayjs";
 dayjs.locale("pl");
 
@@ -10,12 +11,23 @@ export default async (req, res) => {
       //tutaj zmienić datę na zmienną
       // const pyload = req.body;
       // console.log(`GET on backend. `);
+      const session = await getSession({ req });
+      if (!session) {
+        return res.status(401).json({ error: "not_authotized" });
+      }
       const time = await getTime(req.query.id);
       res.status(200).json(time);
 
       break;
     }
     case "POST": {
+      const session = await getSession({ req });
+      if (!session) {
+        return res.status(401).json({ error: "not_authotized" });
+      }
+      if (session.user.role !== "editor") {
+        return res.status(401).json({ error: "permission_denied" });
+      }
       const payload = req.body;
       // console.log("POST payload on backend", payload);
       const time = await saveTimes(payload);
@@ -24,6 +36,13 @@ export default async (req, res) => {
       break;
     }
     case "PUT": {
+      const session = await getSession({ req });
+      if (!session) {
+        return res.status(401).json({ error: "not_authotized" });
+      }
+      if (session.user.role !== "editor") {
+        return res.status(401).json({ error: "permission_denied" });
+      }
       const payload = req.body;
       // console.log("PUT payload on backend", payload);
       const time = await updateTime(req.query.id, payload);
