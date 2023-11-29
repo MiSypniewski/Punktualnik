15f835c8192c39a6f0ff7a7f89c2de66d9484eab
Michal Sypniewski <mi.sypniewski@gmail.com>
2023-11-29 23:25:56 +0100

    fix: session is checked

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -3,15 +3,16 @@ import updateUserPassword from "../../../services/updateUserPassowrd";
 import { getSession } from "next-auth/react";
 // import authorizeUser from "../../../services/authorize";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
   switch (req.method) {
     case "POST": {
       try {
         // console.log(req);
         const session = await getSession({ req });
-        // if (!session) {
-        //   return res.status(401).json({ error: "not_authotized" });
-        // }
+        if (!session) {
+          return res.status(401).json({ error: "not_authotized" });
+        }
 
         const payload = req.body;
         const user = await createUser(payload);
