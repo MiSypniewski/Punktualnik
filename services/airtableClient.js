1878ddaf9d34279e7c3a253eab6372070b25ed29
MiSypniewski <mi.sypniewski@gmail.com>
2025-10-19 15:36:52 +0200

    Update airtableClient.js

--- services/airtableClient.js
+++ services/airtableClient.js
@@ -14,13 +14,14 @@ Airtable.configure({
   apiKey: process.env.AIRTABLE_API_KEY,
 });
 
+const api1Key = process.env.AIRTABLE_API_KEY;
+const api2Key = process.env.AIRTABLE_API_KEY2;
 // Pobierz dzisiejszą datę
 const today = new Date();
 // Pobierz numer dnia miesiąca (od 1 do 31)
 const dayOfMonth = today.getDate();
 
 // Wybierz odpowiednią bazę w zależności od dnia miesiąca
-const baseId =
-  dayOfMonth <= 15 ? process.env.AIRTABLE_BASE : process.env.AIRTABLE_BASE2;
+const baseId = dayOfMonth <= 15 ? api1Key : api2Key;
 
 export default Airtable.base(baseId);
